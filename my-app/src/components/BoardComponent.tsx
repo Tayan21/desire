6ae936bb08a76e632dbfe1280ee10c99cdfbd2c2
Tayan21 <bostybaev@gmail.com>
2022-12-27 17:58:03 +0600

    add chess

---
+++ my-app/src/components/BoardComponent.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useState } from 'react';
+import { Board } from '../models/Board';
+import { Cell } from '../models/Cell';
+import CellComponent from './CellComponent';
+
+interface BoardProps {
+    board: Board;
+    setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({board:Board, setBoard}) => {
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+    
+    function click(cell: Cell) {
+        setSelectedCell(cell);
+    }
+    
+    return (
+        <div className="board">
+            {Board.cells.map((row:Cell[], index:number) =>
+                <React.Fragment key={index}>
+                    {row.map(cell =>
+                        <CellComponent
+                            click={click}
+                            cell={cell}
+                            key={cell.id}
+                            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                        />    
+                    )}
+                </React.Fragment> 
+            )}
+        </div>
+    );
+};
+
+export default BoardComponent;
